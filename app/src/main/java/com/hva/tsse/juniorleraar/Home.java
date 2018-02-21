08ba7c62cdb95c410b1de5feb33f355b8a7ef15e
Melanie Backers <melanie.backers@hva.nl>
2018-02-21 10:33:19 +0100

    Firebase data opgehaald

--- app/src/main/java/com/hva/tsse/juniorleraar/Home.java
+++ app/src/main/java/com/hva/tsse/juniorleraar/Home.java
@@ -2,27 +2,35 @@ package com.hva.tsse.juniorleraar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.hva.tsse.juniorleraar.firebase.Firebase;
+import com.hva.tsse.juniorleraar.model.DialogueCard;
+
+import java.util.List;
 
 public class Home extends AppCompatActivity {
 
     private static final String TAG = "HOMECLASS";
+    private Firebase mFirebase;
+    private List<DialogueCard> mDialogueCards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mFirebase = new Firebase();
+        while (mDialogueCards.size() > 0) {
+            mFirebase.getData();
 
-        TextView text;
-        text = (TextView)findViewById(R.id.textview);
+            mDialogueCards = mFirebase.getmDialogueCards();
+            if(mDialogueCards.size() > 0) {
+                TextView text;
+                text = (TextView) findViewById(R.id.textview);
 
-        Firebase mFirebase = new Firebase();
-        mFirebase.getData();
-        mFirebase.getDialogueCards();
-//        List<DialogueCard> mDialogueCards = mFirebase.getDialogueCards();
-//        Log.w(TAG, "Value3: " + mDialogueCards.get(0).getLevel());
-//        text.setText(mDialogueCards.toString());
+                Log.w(TAG, "Value3: " + mDialogueCards.size());
+            }
+        }
     }
 }
--- app/src/main/java/com/hva/tsse/juniorleraar/firebase/Firebase.java
+++ app/src/main/java/com/hva/tsse/juniorleraar/firebase/Firebase.java
@@ -9,6 +9,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.hva.tsse.juniorleraar.model.DialogueCard;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,52 +17,60 @@ import java.util.List;
  * Created by Melanie on 15-2-2018.
  */
 
-public class Firebase {
+public class Firebase implements Serializable {
     private static final String TAG = "FIREBASECLASS";
     private static final String mURL = "https://junior-leraar.firebaseio.com/Dialoguecard";
     private FirebaseDatabase mDatabase;
     private DatabaseReference mDatabaseReference;
 
-    private final List<DialogueCard> mDialogueCards;
+    private List<DialogueCard> mDialogueCards;
 
+    /**
+     * Creates an instance Firebase and sets the data right
+     */
     public Firebase() {
         mDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mDatabase.getReferenceFromUrl(mURL);
         mDialogueCards = new ArrayList<DialogueCard>();
     }
 
+    /**
+     *  This method gets all the data from the firebase
+     *  and puts all the dialogue cards from the firebase in the list mDialogueCards
+     */
     public void getData(){
-        // Read from the database
+        // Read from the firebase
         mDatabaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                DialogueCard dialogue = new DialogueCard();
+                // In case the data is already catched
+                // list is cleared and catched again
+                clearDialogueCards();
 
+                // For each dialoguecard in firebase
+                // Get it and put it in the list mDialogueCards
                 for (DataSnapshot messageSnapshot : dataSnapshot.getChildren()) {
-                    dialogue = messageSnapshot.getValue(DialogueCard.class);
+                    DialogueCard dialogue = messageSnapshot.getValue(DialogueCard.class);
                     mDialogueCards.add(dialogue);
                 }
-
-                Log.w(TAG, "Value1: " + mDialogueCards.get(0).getLevel());
+                Log.w(TAG, "Trying to get text form firebase: " + mDialogueCards.get(0).getLevel());
+                Log.w(TAG, "Number of object from Firebase: " + mDialogueCards.size());
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
+                // Print error message
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
     }
 
-//    public List<DialogueCard> getDialogueCards(){
-        public void getDialogueCards(){
-//        try{
-//            getData();
-//            Log.w(TAG, "Value2: " + mDialogueCards.get(0).getLevel());
-//            return mDialogueCards;
-//        } catch (Exception e){
-//            Log.w(TAG, e.toString());
-//        }
-//        return null;
+    public void clearDialogueCards(){
+        mDialogueCards.clear();
+    }
+
+    public List<DialogueCard> getmDialogueCards(){
+        return mDialogueCards;
     }
 }
