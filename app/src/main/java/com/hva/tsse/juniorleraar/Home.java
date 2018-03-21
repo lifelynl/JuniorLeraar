94b32c48d87cb1b091a0f57f20943b030e042c55
Julian de Lange <julian.de.lange@hva.nl>
2018-03-21 12:35:28 +0100

    Info

--- app/src/main/java/com/hva/tsse/juniorleraar/Home.java
+++ app/src/main/java/com/hva/tsse/juniorleraar/Home.java
@@ -1,8 +1,13 @@
 package com.hva.tsse.juniorleraar;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -68,27 +73,19 @@ public class Home extends AppCompatActivity {
     }
 
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.action_settings: {
-//                showSettingsActivity(false);
-//                return true;
-//            }
-//            case android.R.id.home: {
-//                onBackPressed();
-//                return true;
-//            }
-//            default: {
-//                return false;
-//            }
-//        }
-//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+                item.setIcon(R.drawable.infoicon);
+                Intent intent = new Intent(Home.this, Info.class);
+                startActivity(intent);
+                return true;
+    }
+
 }
---
+++ app/src/main/java/com/hva/tsse/juniorleraar/Info.java
@@ -0,0 +1,18 @@
+package com.hva.tsse.juniorleraar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Julian on 21-3-2018.
+ */
+
+public class Info extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        this.setTitle("Info");
+    }
+
+}
