16d5a65e896573b9839abd342970e8c04fdac236
Melanie Backers <melanie.backers@hva.nl>
2018-05-23 21:38:40 +0200

    Merge json (worked) into develop (doesnt work)

--- app/src/main/java/com/hva/tsse/juniorleraar/Activity/Home.java
+++ app/src/main/java/com/hva/tsse/juniorleraar/Activity/Home.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
@@ -13,18 +12,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.hva.tsse.juniorleraar.R;
-import com.hva.tsse.juniorleraar.data.DataSource;
-//import com.hva.tsse.juniorleraar.data.Firebase;
+import com.hva.tsse.juniorleraar.data.Json;
 import com.hva.tsse.juniorleraar.model.DialogueCard;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class Home extends AppCompatActivity {
@@ -45,59 +35,13 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         getSupportActionBar().hide();
 
-<<<<<<< HEAD
-//        if (FIRSTRUN) {
-//            Firebase mFirebase = new Firebase();
-//            mFirebase.getData();
-//            Log.w("FIRSTRUN", "FIRSTRUN");
-//            FIRSTRUN = false;
-//        }
-
-=======
->>>>>>> JSON2
         //Get JSON values from JSON file
-        if (FIRSTRUN) {
-            mDialoguecards = new ArrayList<>();
-            try {
-                Log.w(TAG, "BEGIN JSON");
-                JSONObject obj = new JSONObject(loadJSONFromAsset());
-                JSONArray m_jArry = obj.getJSONArray("Dialoguecard");
-                ArrayList<HashMap<String, String>> formList = new ArrayList<HashMap<String, String>>();
-                HashMap<String, String> m_li;
-                Log.w(TAG, obj.toString());
-
-                for (int i = 0; i < m_jArry.length(); i++) {
-                    JSONObject jo_inside = m_jArry.getJSONObject(i);
-                    Log.w(TAG, jo_inside.getString("title"));
-
-                    String theme = jo_inside.getString("theme");
-                    String level = jo_inside.getString("level");
-                    String title = jo_inside.getString("title");
-                    String competence = jo_inside.getString("competence");
-                    String resultText = jo_inside.getString("resultText");
-                    String teacherText = jo_inside.getString("teacherText");
-                    String questionText = jo_inside.getString("questionText");
-
-                    //Add your values in your `ArrayList` as below:
-                    m_li = new HashMap<String, String>();
-                    m_li.put("theme", theme);
-                    m_li.put("level", level);
-                    m_li.put("title", title);
-                    m_li.put("competence", competence);
-                    m_li.put("resultText", resultText);
-                    m_li.put("teacherText", teacherText);
-                    m_li.put("questionText", questionText);
-
-                    mDialoguecards.add(new DialogueCard(theme, level, title, competence, resultText, teacherText, questionText));
-                    formList.add(m_li);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        if (FIRSTRUN){
+            Json json = new Json(this);
+            json.getJSON();
             FIRSTRUN = false;
-            new DataSource(mDialoguecards);
-            Log.w(TAG, mDialoguecards.toString());
         }
+
         // findViews
         Button mDidaBtn = (Button) findViewById(R.id.btn_didactisch);
         Button mColleBtn = (Button) findViewById(R.id.btn_collegiaal);
@@ -142,23 +86,6 @@ public class Home extends AppCompatActivity {
                 Home.this.startActivity(info);
             }
         });
-    }
-
-    public String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream is = this.getAssets().open("junior-leraar.json");
-            Log.w(TAG, is.toString());
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
     }
 
     @Override
